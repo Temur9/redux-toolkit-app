00fa2b72ddcca855c9590cf8d90328317540e31c
Temur <99079769+Temur9@users.noreply.github.com>
2023-02-28 18:58:46 +0500

    added loader in mainpage

---
+++ src/UI/loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <>
+        <div className="spinner-border d-block mx-auto" role='status'>
+            <span className="visually-hidden">Loading...</span>
+        </div>
+    </>
+  )
+}
+
+export default Loader
